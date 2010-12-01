db44d3c77eab76d4dc06f07b452b43b2be3406fc
Yuan Sheng <jeep.pig@gmail.com>
2010-12-02 02:12:08 +0800

    测试提交2

---
+++ DFWEditor_Alpha/G.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DFWEditor_Alpha
+{
+    public static class G
+    {
+        public static String Ver = "Editor Alpha  ";
+        public static bool bAreaBrush;
+        public static bool bGrid;
+    }
+}
