642c6c3e3720ea6331efbaebaeca3043ed767b38
Yuan Sheng <pigmon@163.com>
2010-12-06 07:34:23 +0800

    BrushData遗漏文件

---
+++ DFWEditor_Alpha/BrushData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DFWEditor_Alpha
+{
+    public class BrushData
+    {
+        public PopZone texture;
+        public int index;
+    }
+}
